9292d96813bfd13d9e9d3b97afc17bf9fae03dcb
Tripurari-singh <tripurari5821@gmail.com>
2025-07-07 23:53:10 +0530

    First Commit

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,65 @@
 import  express  from "express";
-import z from "zod"
+import z from "zod";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {CreateUserSchema , SigninSchema , CreateRoomSchema , } from "@repo/common/types"
 
 const app = express();
 app.use(express.json());
 app.post("/signup" , (req , res) => {
-    const signupSchema = z.object({
-        username : z.string(),
-        password : z.string()
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return
+    }
+
+    //DB Call
+
+    res.json({
+        message: "User Created",
+        userId : "123"
+    })
+    
+})
+
+app.post("/signin" , (req , res) => {
+    const data = SigninSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message : "Incorrect Crediantials"
+        })
+        return
+    }
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    } , JWT_SECRET)
+
+    res.json({
+        message : "Successfylly Signed In",
+        token : token
+    })
+})
+
+app.post("/room" , (req , res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Crediantials"
+        })
+        return
+    }
+
+    //DB Call
+    res.json({
+        message : "Room Created",
+        roomId : 123
     })
 
-    const { username , password } = signupSchema.parse(req.body);
 
-    
 })
 
 app.listen(3000 , () => {
