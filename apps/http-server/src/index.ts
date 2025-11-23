63a660d2024a477dd8cc654c16c6da7230f3aaca
Tripurari-singh <tripurari5821@gmail.com>
2025-11-23 22:37:17 +0530

    Initialised ws,http servers

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/" , (req , res) => {
+    res.json({
+        message : "hello"
+    })
+})
+
+app.listen(3000 , () => {
+    console.log("server is listening on port 3000 !!")
+})
