6b2e9b738488d6ae6edbce95ecbe69c9094c0ec4
Tripurari-singh <tripurari5821@gmail.com>
2025-11-24 16:59:26 +0530

    http-server Routed Added

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,11 +1,74 @@
 import express from "express";
+import z from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
 
 const app = express();
 
-app.get("/" , (req , res) => {
-    res.json({
-        message : "hello"
+app.get("/signup" , (req  , res) => {
+    try{
+        const signupschema = z.object({
+        username : z.string().max(20 , "username must be atmax 20 characters").min(3 , "username must be atleast 3 characters"),
+        password : z.string().max(20 , "password must be atmax 20 characters").min(3 , "password must be atleast 3 characters"),
+        email : z.string().email(),
     })
+     
+    const data = signupschema.parse(req.body);
+    
+    if(!data){
+        res.status(400).json({
+            message : "Data Not found"
+        })
+    }
+
+    // DB call to get check use already exist...
+
+    // DB call to add user to data
+
+    res.status(200).json({
+        message : "user added successfully !"
+    })
+    }catch(error){
+        res.status(500).json({
+            message : "Internal server Error",
+            error  : error
+        })
+    }
+})
+app.post("/signup" , (req , res) => {
+    try{
+            const signinschema = z.object({
+        username : z.string().max(20 , "username must be atmax 20 characters").min(3 , "username must be atleast 3 characters"),
+        password : z.string().max(20 , "password must be atmax 20 characters").min(3 , "password must be atleast 3 characters"),
+    })
+
+    const data = signinschema.parse(req.body);
+    
+    if(!data){
+        res.status(400).json({
+            message : "Data Not found"
+        })
+    }
+
+    //DB call to get the user
+    const userId = 1
+    const token = jwt.sign({userId} , JWT_SECRET)
+
+    res.status(200).json({
+        message : "SignIn Successfully",
+        token : token
+    })
+    }catch(error){
+            res.status(500).json({
+            message : "Internal server Error",
+            error  : error
+        })
+    }
+})
+
+app.post("/room" , (req , res) => {
+    
 })
 
 app.listen(3000 , () => {
---
+++ apps/http-server/src/middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "./config";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+    if (!decoded || !decoded.userId) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+     
+    //@ts-ignore
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
