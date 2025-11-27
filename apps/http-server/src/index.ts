a14edce1245fe49d05eb8cc017f87684c485ab65
Tripurari-singh <tripurari5821@gmail.com>
2025-11-27 16:35:41 +0530

    http-server Running

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -10,99 +10,99 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/signup" , async (req  , res) => {
-//     try{
+app.get("/signup" , async (req  , res) => {
+    try{
      
-//     const ParsedData = CreateUserSchema.parse(req.body);
+    const ParsedData = CreateUserSchema.parse(req.body);
     
-//     if(!ParsedData){
-//         res.status(400).json({
-//             message : "Invalid crediantials"
-//         })
-//         return; 
-//     }
+    if(!ParsedData){
+        res.status(400).json({
+            message : "Invalid crediantials"
+        })
+        return; 
+    }
 
-//     // DB call to get check use already exist...
-//     const userExist = await prisma.user.findUnique({
-//         where : {
-//             email : ParsedData.email
-//         }
-//     })
+    // DB call to get check use already exist...
+    const userExist = await prisma.user.findUnique({
+        where : {
+            email : ParsedData.email
+        }
+    })
 
-//     if(userExist){
-//         res.status(400).json({
-//             message : "User Already Exist"
-//         })
-//         return;
-//     }
+    if(userExist){
+        res.status(400).json({
+            message : "User Already Exist"
+        })
+        return;
+    }
 
-//     // DB call to add user to data
-//     const user =  await prisma.user.create({
-//         data : {
-//             username : ParsedData.username,
-//             password : ParsedData.password,
-//             email : ParsedData.email,
-//             avatar : ParsedData.avatar,
-//         }
-//     })
+    // DB call to add user to data
+    const user =  await prisma.user.create({
+        data : {
+            username : ParsedData.username,
+            password : ParsedData.password,
+            email : ParsedData.email,
+            avatar : ParsedData.avatar,
+        }
+    })
 
-//     res.status(200).json({
-//         message : "user added successfully !",
-//         userid : user.id,
-//         user,
-//     })
-//     }catch(error){
-//         res.status(500).json({
-//             message : "Internal server Error",
-//             error  : error
-//         })
-//     }
-// })
-// app.post("/signin" , async (req , res) => {
-//     try{
+    res.status(200).json({
+        message : "user added successfully !",
+        userid : user.id,
+        user,
+    })
+    }catch(error){
+        res.status(500).json({
+            message : "Internal server Error",
+            error  : error
+        })
+    }
+})
+app.post("/signin" , async (req , res) => {
+    try{
 
-//     const ParsedData = SigninSchema.parse(req.body);
+    const ParsedData = SigninSchema.parse(req.body);
     
-//     if(!ParsedData){
-//         res.status(400).json({
-//             message : "Invalid crediantials"
-//         })
-//         return;
-//     }
+    if(!ParsedData){
+        res.status(400).json({
+            message : "Invalid crediantials"
+        })
+        return;
+    }
 
-//     //DB call to get the user
-//     const userId = 1
-//     const token = jwt.sign({userId} , JWT_SECRET)
+    //DB call to get the user
+    const userId = 1
+    const token = jwt.sign({userId} , JWT_SECRET)
 
-//     res.status(200).json({
-//         message : "SignIn Successfully",
-//         token : token
-//     })
-//     }catch(error){
-//             res.status(500).json({
-//             message : "Internal server Error",
-//             error  : error
-//         })
-//     }
-// })
+    res.status(200).json({
+        message : "SignIn Successfully",
+        token : token
+    })
+    }catch(error){
+            res.status(500).json({
+            message : "Internal server Error",
+            error  : error
+        })
+    }
+})
 
-// app.post("/room", middleware , (req , res) => {
-//     try{
-//         const data = CreateRoomSchema.parse(req.body);
+app.post("/room", middleware , (req , res) => {
+    try{
+        const data = CreateRoomSchema.parse(req.body);
 
-//         if(!data){
-//             res.status(400).json({
-//                 message : "Invalid crediantials"
-//             })
-//         }
+        if(!data){
+            res.status(400).json({
+                message : "Invalid crediantials"
+            })
+        }
 
         
-//     }catch(error){
-//         res.status(500).json({
-//             message : "Internal server Error"
-//         })
-//     }
-// })
+    }catch(error){
+        res.status(500).json({
+            message : "Internal server Error"
+        })
+    }
+})
 
 app.get("./hello" , async (req , res) => {
     res.json({
