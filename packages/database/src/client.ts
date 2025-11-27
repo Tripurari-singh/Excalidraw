8cba521fa2e07645968770d216b6e2b1d5cb8816
Tripurari-singh <tripurari5821@gmail.com>
2025-11-27 14:37:58 +0530

    export/import issue Resolved for now 26_nov

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
@@ -13,3 +14,4 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
