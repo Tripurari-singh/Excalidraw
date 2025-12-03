a3d4212c91179fc8b40bc6f2b6e8289a56e420c2
Tripurari-singh <tripurari5821@gmail.com>
2025-12-03 23:06:46 +0530

    can add persisting Rectangles

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -2,6 +2,19 @@
 import { start } from "node:repl";
 import { useEffect, useRef } from "react"
 
+type Shape = {
+    type : "rectangle",
+    x : number,
+    y : number,
+    width : number,
+    height : number,
+} | {
+    type : "circle",
+    centerX : number,
+    centerY : number,
+    radius : number
+}
+
 export default function Canvas(){
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -12,14 +25,19 @@ export default function Canvas(){
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d");
 
+            let existingShapes : Shape[] = [];
+
             if(!ctx){
                 return;
             }
 
+            ctx.fillStyle = "rgba(0,0,0)"; 
+            ctx.fillRect(0 , 0 , canvas.width , canvas.height);
+
             // ctx.strokeRect(25 , 25 , 100 , 100);
 
 
-            let clicked = true;
+            let clicked = false;
             let startX = 0;
             let startY = 0;
 
@@ -27,26 +45,51 @@ export default function Canvas(){
                 clicked = true;
                  startX = e.clientX;
                  startY = e.clientY;
-                console.log(e.clientX);
-                console.log(e.clientX);
             })
 
             canvas.addEventListener("mouseup" , (e) => {
                 clicked = false;
-                console.log(e.clientX);
-                console.log(e.clientX);
+                const width = e.clientX - startX;
+                const height = e.clientY - startY;
+
+                existingShapes.push({
+                    type : "rectangle",
+                    x : startX,
+                    y : startY,
+                    height,
+                    width
+                })
             })
 
             canvas.addEventListener("mousemove" , (e) => {
                 if(clicked){
                     const width = e.clientX - startX;
                     const height = e.clientY - startY;
-                    ctx.clearRect(0 ,0 ,canvas.width , canvas.height);
+                    clearCanvas(existingShapes ,ctx , canvas );
+                    ctx.strokeStyle = ("rgba(255 , 255 , 255)")
                     ctx.strokeRect(startX , startY , width , height);
                 }
             })
 
         }
+
+
+        function clearCanvas(existingShapes : Shape[] , ctx : CanvasRenderingContext2D , canvas : HTMLCanvasElement){
+                    ctx.clearRect(0 ,0 ,canvas.width , canvas.height);
+                    ctx.fillStyle = "rgba(0 , 0 , 0)";
+                    ctx.fillRect(0 ,0 ,canvas.width , canvas.height);
+
+                    existingShapes.map((shape) => {
+                        if(shape.type === "rectangle"){
+                            ctx.strokeStyle = "rgba(255 , 255 , 255)";
+                            ctx.strokeRect(shape.x , shape.y , shape.width , shape.height);
+                        }
+                    })
+                    console.log(existingShapes);
+        }
+
+
+
     } , [canvasRef])
 
     return <div>
