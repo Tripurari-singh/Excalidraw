2a8b5930d7b4813eec96d2ff9d5f2634132b2fe0
Tripurari-singh <tripurari5821@gmail.com>
2025-12-10 12:57:40 +0530

    Restarting Canvas

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,18 +1,25 @@
 "use client"
-import initDraw from "@/app/draw/page";
-import { start } from "node:repl";
-import { useEffect, useRef } from "react"
 
+import { useEffect, useRef } from "react"
 
 export default function canvas(){
 
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     useEffect(() => {
         if(canvasRef.current){
-            initDraw(canvasRef.current);
+            const canvas = canvasRef.current;
+            if(!canvas){
+                return;
+            }
+
+            const ctx = canvas.getContext("2d");
+            if(!ctx){
+                return;
+            }
+            ctx.strokeRect(25 , 25 , 100 , 100);
         }
-    } , [canvasRef])
+    } , [canvasRef]);
 
     return <div>
         <canvas ref={canvasRef} width={2000} height={2000}></canvas>
